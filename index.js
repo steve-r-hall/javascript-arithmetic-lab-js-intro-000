6935d3db67c807587857b3da3d35baff56bc7dce
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-18 11:02:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  var sum = a + b
+  return sum
+}
+
+function subtract(a, b) {
+  var reslt = (a - b)
+  return reslt 
+}
+
+function multiply(a, b) {
+  var sum = (a * b)
+  return sum
+}
+
+function divide(a, b) {
+  var dividend = (a / b)
+  return dividend
+}
